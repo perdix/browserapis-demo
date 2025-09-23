d50f72b3fd451a4ce87616a65a21babfdfbb5055
Paul Opitz <paul.opitz@outlook.com>
2025-09-23 19:06:29 +0200

    Test

--- src/routes/sensors/+page.svelte
+++ src/routes/sensors/+page.svelte
@@ -35,7 +35,7 @@
             if (resp !== "granted") return; 
         }
 
-        window.addEventListener("deviceorientation", orient);
+        window.addEventListener("deviceorientationabsolute", orient);
     }
 
 
